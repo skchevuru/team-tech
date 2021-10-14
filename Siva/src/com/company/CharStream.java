846c66917725ff131986e4c2d6204bde090e8168
skchevuru <sivakishore.chevuru@gmail.com>
2021-10-14 08:24:54 -0400

    added char array logic

--- Siva/src/com/company/CharStream.java
+++ Siva/src/com/company/CharStream.java
@@ -2,18 +2,22 @@ package com.company;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class CharStream {
     public static void main(String args[]){
-        LocalDateTime localDateTime = LocalDateTime.now();
         String input = "aabbbcccabcaaaaaccbb";
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        repeatedCharStringChar(input);
+        System.out.println("Time Taken for char array = "+ localDateTime.until(LocalDateTime.now(), ChronoUnit.MILLIS));
+
+        localDateTime = LocalDateTime.now();
         repeatedCharStrings(input);
-        System.out.println("Time Taken "+ localDateTime.until(LocalDateTime.now(), ChronoUnit.MILLIS));
+        System.out.println("Time Taken for bufferstring =  "+ localDateTime.until(LocalDateTime.now(), ChronoUnit.MILLIS));
     }
 
-
+    //Using Buffer String
     private static void repeatedCharStrings(String input){
         char[] charInput = input.toCharArray();
         char compare = charInput[0];
@@ -28,8 +32,9 @@ public class CharStream {
                 stringBuffer.append(charInput[i]);
             }else{
                 compare = charInput[i];
-                if(highest < stringBuffer.toString().length()){
-                    finalString = stringBuffer.toString();
+                String previouString = stringBuffer.toString();
+                if(highest < previouString.length()){
+                    finalString = previouString;
                     highest = finalString.length();
                     finalLocation = location;
                 }
@@ -43,4 +48,54 @@ public class CharStream {
         System.out.println("Final String Location = "+(finalLocation+1));
 
     }
+    //Using char array logic
+    private static void repeatedCharStringChar(String input){
+        char[] charInput = input.toCharArray();
+        char compare = charInput[0];
+        char[] stringBuffer = new char[input.length()];
+        stringBuffer[0] = compare;
+        int highest = 0;
+        int location = 0;
+        int finalLocation = 0;
+        char[] finalString = new char[input.length()];;
+        int finalStringLength = 0;
+        int counter = 1;
+        for(int i=1; i<charInput.length; i++){
+            if(compare == charInput[i]){
+                stringBuffer[counter] = charInput[i];
+                counter++;
+
+            }else{
+                compare = charInput[i];
+                char[] previouString = stringBuffer;
+                finalStringLength = findLength(previouString);
+                if(highest < finalStringLength){
+                    finalString = previouString;
+                    highest = finalStringLength;
+                    finalLocation = location;
+                }
+                location = i;
+                stringBuffer = new char[input.length()];
+                counter = 0;
+                stringBuffer[counter]=compare;
+            }
+        }
+
+        System.out.println("Final String = "+new String(finalString).trim());
+        System.out.println("Final String Location = "+(finalLocation+1));
+
+    }
+
+    private static int findLength(char[] charArray){
+        for(int i=0;i<charArray.length;i++){
+            if(charArray[i] == 0){
+                return i;
+            }
+        }
+        return charArray.length;
+    }
+
+    //private static String convertString(char[] charArray)
+
+
 }
